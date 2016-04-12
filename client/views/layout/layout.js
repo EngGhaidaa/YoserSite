7b31f3f4785773e7a801241a1f633443324f8fa4
EngGhaidaa <mylovegh348@gmail.com>
2016-04-12 15:34:56 +0300

    add ??

--- client/views/layout/layout.js
+++ client/views/layout/layout.js
@@ -1,43 +1,77 @@
 //ToDo read code
+//Template.layout.rendered = function () {
+//    //scroll to anchor
+//    $('body').on('click', 'a', function (e) {
+//        var href = $(this).attr("href");
+//       // var hash = href.substring(1);
+//        if (!href) {
+//            return href;
+//        }
+//        if (href.length > 1 && href.charAt(0) == "#")
+//        {
+//            var hash = href.substring(1);
+//            //substring (cut)
+//            if (hash)
+//            {
+//                e.preventDefault();
+//                //إيقاف جميع العمليات لانهي عمليتي بخبرك شو تعمل
+//               var offset = $('*[id="' + hash + '"]');
+//
+//                if (offset)
+//                {
+//                   $('html,body').animate({scrollTop: offset.top}, 400);
+//
+//                }
+//            }
+//        }
+//        else {
+//            if (href.indexOf("http://") != 0 && href.indexOf("https://") != 0 && href.indexOf("#") != 0)
+//            {
+//                $('html,body').scrollTop();
+//            }
+//        }
+//    });
+//    /*TEMPLATE_RENDERED_CODE*/
+//};
 Template.layout.rendered = function() {
-	 //scroll to anchor
-	$('body')
-	$('body').on('click', 'a', function(e) {
-		var href = $(this).attr("href");
-		if(!href) {
-			return;
-		}
-		if(href.length > 1 && href.charAt(0) == "#") {
-			var hash = href.substring(1);
-			if(hash) {
-				e.preventDefault();
+    // scroll to anchor
+    $('body').on('click', 'a', function(e) {
+        var href = $(this).attr("href");
+        if(!href) {
+            return;
+        }
+        if(href.length > 1 && href.charAt(0) == "#") {
+            var hash = href.substring(1);
+            if(hash) {
+                e.preventDefault();
 
-				var offset = $('*[id="' + hash + '"]').offset();
+                var offset = $('*[id="' + hash + '"]').offset();
 
-				if (offset) {
-					$('html,body').animate({ scrollTop: offset.top - 60 }, 400);
-				}
-			}
-		} else {
-			if(href.indexOf("http://") != 0 && href.indexOf("https://") != 0 &&
-href.indexOf("#") != 0) {
-				$('html,body').scrollTop(0);
-			}
-		}
-	});
-	/*TEMPLATE_RENDERED_CODE*/
+                if (offset) {
+                    $('html,body').animate({ scrollTop: offset.top - 60 }, 400);
+                }
+            }
+        } else {
+            if(href.indexOf("http://") != 0 && href.indexOf("https://") != 0 && href.indexOf("#") != 0) {
+                $('html,body').scrollTop(0);
+            }
+        }
+    });
+    /*TEMPLATE_RENDERED_CODE*/
 };
 
 Template.layout.events({
-    "click": function(event) { // Fix Bootstrap Dropdown Menu Collapse on click outside Menu
+    "click": function (event) { // Fix Bootstrap Dropdown Menu Collapse on click outside Menu
         var clickover = $(event.target).closest(".dropdown-toggle").length;
         var opened = $(".navbar-collapse").hasClass("in");
         if (opened === true && !clickover) {
             $('.navbar-collapse').collapse('hide');
         }
     },
-
-    "keyup": function(event) {
+    //'click .menu-item-simple': function (event) {
+    //   $(event.target).find('a');
+    //},
+    "keyup": function (event) {
         if (event.keyCode === 27) { // Bootstrap Dropdown Menu Collapse on ESC pressed
             var opened = $(".navbar-collapse").hasClass("in");
             if (opened === true) {
@@ -48,30 +82,28 @@ Template.layout.events({
 });
 
 
-Template.FreeLayoutMainMenu.rendered = function() {
-	$(".menu-item-collapse .dropdown-toggle").each(function() {
-		if($(this).find("li.active")) {
-			$(this).removeClass("collapsed");
-		}
-		$(this).parent().find(".collapse").each(function() {
-			if($(this).find("li.active").length) {
-				$(this).addClass("in");
-			}
-		});
-	});
+Template.FreeLayoutMainMenu.rendered = function () {
+    $(".menu-item-collapse .dropdown-toggle").each(function () {
+        if ($(this).find("li.active")) {
+            $(this).removeClass("collapsed");
+        }
+        $(this).parent().find(".collapse").each(function () {
+            if ($(this).find("li.active").length) {
+                $(this).addClass("in");
+            }
+        });
+    });
 
-	$("body").css({ "position": "relative" });
-	$("body").attr({ "data-spy": "scroll", "data-target": "#menu", "data-offset": "70" });
+    $("body").css({"position": "relative"});
+    $("body").attr({"data-spy": "scroll", "data-target": "#menu", "data-offset": "70"});
 };
 
 Template.FreeLayoutMainMenu.events({
-	"click .toggle-text": function(e, t) {
-		e.preventDefault();
-		$(e.target).closest("ul").toggleClass("menu-hide-text");
-	}
+    "click .toggle-text": function (e, t) {
+        e.preventDefault();
+        $(e.target).closest("ul").toggleClass("menu-hide-text");
+    }
 
 });
 
-Template.FreeLayoutMainMenu.helpers({
-
-});
+Template.FreeLayoutMainMenu.helpers({});
